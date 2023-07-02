3dfea0edc6d88488c6ecc2f96bfcbf968531ccc2
Akash M <108210779+akashikur@users.noreply.github.com>
2023-07-02 12:37:07 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,3 +1,22 @@
-//your JS code here. If required.
-const li=documen.getElementById("level");
-alert("The level of the element is: 4");
+// script.js
+
+// Retrieve the list item element with the ID "level"
+var levelElement = document.getElementById("level");
+
+// Check if the element exists
+if (levelElement) {
+  // Retrieve the level of the element by counting its parent list items
+  var level = 0;
+  var parent = levelElement.parentNode;
+  while (parent && parent.nodeName !== "BODY") {
+    if (parent.nodeName === "LI") {
+      level++;
+    }
+    parent = parent.parentNode;
+  }
+
+  // Display the level in an alert box
+  alert("The level of the element is: " + level);
+} else {
+  alert("The element with ID 'level' was not found.");
+}
